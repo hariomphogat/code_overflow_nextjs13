06890af58b066dea1ec9a455436b358e19eb97cb
hariomphogat <hariom9728034346@gmail.com>
2023-09-30 19:59:00 +0530

    themeProvider_useeffect warning resolve

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,19 +10,17 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
-  const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    }
-    if (mode === "light") {
-      setMode("dark");
-      document.documentElement.classList.add("dark");
-    }
-  };
-  // handleThemeChange();
-  // to be resolved
   useEffect(() => {
+    const handleThemeChange = () => {
+      if (mode === "dark") {
+        setMode("light");
+        document.documentElement.classList.add("light");
+      }
+      if (mode === "light") {
+        setMode("dark");
+        document.documentElement.classList.add("dark");
+      }
+    };
     handleThemeChange();
   }, [mode]);
 
