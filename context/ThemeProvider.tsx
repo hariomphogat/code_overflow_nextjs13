f4e802170b16e35daff17edff99f71fd79b7a3c1
hariomphogat <hariom9728034346@gmail.com>
2023-10-01 18:07:11 +0530

    add navbar mobNav & resolve themeProvider errors

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,22 +10,23 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
-  const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    }
-    if (mode === "light") {
-      setMode("dark");
-      document.documentElement.classList.add("dark");
-    }
-  };
-  // handleThemeChange();
-  // to be resolved
   useEffect(() => {
+    const handleThemeChange = () => {
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme:dark)").matches)
+      ) {
+        setMode("dark");
+        document.documentElement.classList.add("dark");
+      } else {
+        setMode("light");
+        document.documentElement.classList.remove("dark");
+      }
+    };
     handleThemeChange();
   }, [mode]);
-
+  console.log("MODE:", mode);
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
       {children}
