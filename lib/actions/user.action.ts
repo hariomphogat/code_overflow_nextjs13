5272e8d728cd3a77e27c5a0632f7b7c5e0c33d9b
hariomphogat <hariom9728034346@gmail.com>
2023-11-10 21:11:50 +0530

    remove var unused warning

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -63,6 +63,7 @@ export async function deleteUser(params: DeleteUserParams) {
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       "_id"
     );
+    console.log(userQuestionIds);
     // delete user questions
     await Question.deleteMany({ author: user._id });
 
