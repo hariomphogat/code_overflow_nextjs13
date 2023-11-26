f486ca046651cdc2e0d1bc2c0a71eb9f20d20a3a
hariomphogat <hariom9728034346@gmail.com>
2023-11-26 17:22:08 +0530

    add community and tags page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
   UpdateUserParams,
 } from "./shared.types";
 import Question from "../models/question.model";
@@ -87,3 +88,27 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+// get all users from database
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+    const users = await User.find({}).sort({ createdAt: -1 });
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params:GetAllUsersParams){
+//     try {
+//       connectToDatabase();
+
+//     } catch (error) {
+//   console.log(error);
+//   throw error;
+//     }
+//   }
